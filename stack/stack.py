ad95e5e3ee0a53f2bf55c791ba41fb900464fdf9
Tiffany Crosby <tiffany.crosby65@gmail.com>
2020-07-15 10:14:28 -0400

    queue and stack passing tests.

--- stack/stack.py
+++ stack/stack.py
@@ -1,3 +1,10 @@
+# import sys
+# sys.path.append('../singly_linked_list')
+# from singly_linked_list import LinkedList
+
+from single_linked_list import LinkedList
+
+
 """
 A stack is a data structure whose primary purpose is to store and
 return elements in Last In First Out order. 
@@ -13,13 +20,21 @@ return elements in Last In First Out order.
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        # self.storage = []
+        self.storage = LinkedList()
 
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+        # self.storage.append(value) #adding values to the list
+        self.size += 1 #incrementing size so when we ask for lenth we get the correct answer
+        self.storage.add_to_tail(value)
+
 
     def pop(self):
-        pass
+        if self.size == 0:
+            return None
+
+        self.size -= 1
+        return self.storage.remove_tail()
